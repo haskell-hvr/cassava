579f5c739e18af9099ddaeba2925d650c6d52bf7
Johan Tibell <johan.tibell@gmail.com>
2012-03-29 19:21:41 -0700

    Remove unused imports

--- Data/Ceason.hs
+++ Data/Ceason.hs
@@ -23,22 +23,9 @@ module Data.Ceason
     , (.!)
     ) where
 
-import Control.Applicative
-import Data.Attoparsec.Char8 (double, number, parseOnly)
-import qualified Data.ByteString as B
-import qualified Data.ByteString.Char8 as B8
 import qualified Data.ByteString.Lazy as L
-import Data.Int
-import qualified Data.Text as T
-import qualified Data.Text.Encoding as T
-import qualified Data.Text.Lazy as LT
-import qualified Data.Text.Lazy.Encoding as LT
 import Data.Traversable
-import Data.Vector (Vector, (!))
-import qualified Data.Vector as V
-import Data.Word
-import GHC.Float (double2Float)
-import Prelude hiding (takeWhile)
+import Data.Vector (Vector)
 
 import Data.Ceason.Encode
 import Data.Ceason.Parser.Internal
