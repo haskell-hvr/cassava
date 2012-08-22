4b8b923a4b36471f52fe6aa6f6516f56f8f16e82
Johan Tibell <johan.tibell@gmail.com>
2012-08-21 22:19:54 -0700

    Move all encoding and decoding into one module

--- Data/Ceason.hs
+++ Data/Ceason.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE BangPatterns, Rank2Types #-}
-
 -- | RFC 4180 compliant CSV parsing and encodig.
 module Data.Ceason
     (
@@ -56,14 +54,7 @@ module Data.Ceason
     , namedRecord
     ) where
 
-import Control.Applicative
-import qualified Data.ByteString as B
-import qualified Data.ByteString.Lazy as L
-import Data.Traversable
-import Data.Vector (Vector)
-
-import Data.Ceason.Encode
-import Data.Ceason.Parser.Internal hiding (record)
+import Data.Ceason.Encoding
 import Data.Ceason.Types
 
 -- $typeconversion
@@ -87,60 +78,3 @@ import Data.Ceason.Types
 -- as the header. Name-based conversion is more robust to changes in
 -- the file structure e.g. to reording or addition of columns, but can
 -- be a bit slower.
-
--- | Efficiently deserialize CSV records from a lazy 'L.ByteString'.
--- If this fails due to incomplete or invalid input, @'Left' msg@ is
--- returned. Equivalent to @'decodeWith' 'defaultDecodeOptions'@.
-decode :: FromRecord a => L.ByteString -> Either String (Vector a)
-decode = decodeWith defaultDecodeOptions
-{-# INLINE decode #-}
-
--- | Efficiently deserialize CSV records from a lazy 'L.ByteString'.
--- If this fails due to incomplete or invalid input, @'Left' msg@ is
--- returned. The data is assumed to be preceeded by a header.
--- Equivalent to @'decodeByNameWith' 'defaultDecodeOptions'@.
-decodeByName :: FromNamedRecord a => L.ByteString
-                 -> Either String (Header, Vector a)
-decodeByName = decodeByNameWith defaultDecodeOptions
-{-# INLINE decodeByName #-}
-
-------------------------------------------------------------------------
--- ** Encoding and decoding options
-
--- $options
---
--- The 'decodeWith' and 'decodeByNameWith' functions lets you
--- customize how the CSV data is parsed. These can be used to e.g.
--- parse tab-separated data.
-
--- | Default decoding options:
---
---  * 'decDelimiter': comma
-defaultDecodeOptions :: DecodeOptions
-defaultDecodeOptions = DecodeOptions
-    { decDelimiter = 44  -- comma
-    }
-
--- | Like 'decode', but lets you customize how the CSV data is parsed.
-decodeWith :: FromRecord a => DecodeOptions -> L.ByteString
-           -> Either String (Vector a)
-decodeWith !opts = decodeWithP (csv opts) (parse . traverse parseRecord)
-{-# INLINE [1] decodeWith #-}
-
-{-# RULES
-    "idDecodeWith" decodeWith = idDecodeWith
- #-}
-
--- | Same as 'decodeWith', but more efficient as no type
--- conversion is performed.
-idDecodeWith :: DecodeOptions -> L.ByteString
-             -> Either String (Vector (Vector B.ByteString))
-idDecodeWith !opts = decodeWithP (csv opts) pure
-
--- | Like 'decodeByName', but lets you customize how the CSV data is
--- parsed.
-decodeByNameWith :: FromNamedRecord a => DecodeOptions -> L.ByteString
-                     -> Either String (Header, Vector a)
-decodeByNameWith !opts =
-    decodeWithP (csvWithHeader opts)
-    (\ (hdr, vs) -> (,) <$> pure hdr <*> (parse $ traverse parseNamedRecord vs))
