b392fc6ada9a4437616eb74a85243966cc9b5e94
Simon Meier <iridcode@gmail.com>
2012-08-24 12:13:25 +0300

    Make example in README.md standalone
    Also add an examples/ directory.

--- examples/IndexBasedDecode.hs
+++ examples/IndexBasedDecode.hs
@@ -1,21 +1,3 @@
-# cassava: A CSV parsing and encoding library
-
-A CSV parsing and encoding library optimized for ease of use and high
-performance.
-
-## Usage example
-
-Here's the two second crash course in using the library. Given a CSV
-file with this content:
-
-```
-John Doe,50000
-Jane Doe,60000
-```
-
-here's how you'd process it record-by-record:
-
-```haskell
 {-# LANGUAGE ScopedTypeVariables #-}
 
 import qualified Data.ByteString.Lazy as BL
@@ -29,4 +11,3 @@ main = do
         Left err -> putStrLn err
         Right v -> V.forM_ v $ \ (name, salary :: Int) ->
             putStrLn $ name ++ " earns " ++ show salary ++ " dollars"
-```
