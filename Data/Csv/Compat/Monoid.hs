0091ae4720b1a9a7eaf0316918771589da570ad7
Johan Tibell <johan.tibell@gmail.com>
2012-08-29 18:08:56 -0700

    Add missing file

---
+++ Data/Csv/Compat/Monoid.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE CPP #-}
+module Data.Csv.Compat.Monoid
+    ( (<>)
+    ) where
+
+import Data.Monoid
+
+#if !MIN_VERSION_base(4,5,0)
+infixr 6 <>
+-- | An infix synonym for 'mappend'.
+(<>) :: Monoid m => m -> m -> m
+(<>) = mappend
+{-# INLINE (<>) #-}
+#endif
