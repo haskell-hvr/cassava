974a380b6f279754bcc2c967f792666845dc1ae6
Johan Tibell <johan.tibell@gmail.com>
2012-09-19 20:03:04 +0200

    Get rid of lazy parser
    Redesign incremental parsing interface

--- Data/Csv/Streaming.hs
+++ Data/Csv/Streaming.hs
@@ -3,23 +3,18 @@
 module Data.Csv.Streaming
     (
     -- * Continuation based parsing
-      decode
+      decodeHeader
+    , decodeHeaderWith
+    , HeaderParser(..)
+    , decode
     , decodeWith
     , Parser(..)
-    , RecordParser(..)
-
-    -- * Lazy parsing
-    , RecordStream(..)
-    , Result(..)
-    , lazyDecode
-    , lazyDecodeWith
     ) where
 
 import Control.Applicative
 import qualified Data.Attoparsec as A
 import Data.Attoparsec.Char8 (endOfLine)
 import qualified Data.ByteString as B
-import qualified Data.ByteString.Lazy as BL
 import qualified Data.Vector as V
 import Text.Show.Functions ()
 
@@ -30,46 +25,49 @@ import Data.Csv.Types
 ------------------------------------------------------------------------
 -- * Continuation based parsing
 
-data Parser a = FailH B.ByteString String -- TODO: add more failure info
-              | PartialH (B.ByteString -> Parser a)
-              | DoneH Header (RecordParser a)
+-- TODO: Write custom Show instance to avoid re-exporting orphan
+-- instance.
+
+data HeaderParser = FailH B.ByteString String -- TODO: add more failure info
+                  | PartialH (B.ByteString -> HeaderParser)
+                  | DoneH Header B.ByteString
+                  deriving Show
+
+decodeHeader :: B.ByteString -> HeaderParser
+decodeHeader = decodeHeaderWith defaultDecodeOptions
+
+decodeHeaderWith :: DecodeOptions -> B.ByteString -> HeaderParser
+decodeHeaderWith !opts = go . parser
+  where
+    parser = A.parse (header $ decDelimiter opts)
+
+    go (A.Fail rest _ msg) = FailH rest err
+      where err = "parse error (" ++ msg ++ ")"
+    -- TODO: Check empty and give attoparsec one last chance to return
+    -- something:
+    go (A.Partial k)       = PartialH $ \ s -> go (k s)
+    go (A.Done rest r)     = DoneH r rest
+
+data Parser a = Fail B.ByteString String -- TODO: add more failure info
+              | Partial (B.ByteString -> Parser a)
+              | Some [Either String a] (B.ByteString -> Parser a)
+              | Done [Either String a]
               deriving Show
 
-data RecordParser a = Fail B.ByteString String -- TODO: add more failure info
-                    | Partial (B.ByteString -> RecordParser a)
-                    | Some a (B.ByteString -> RecordParser a)
-                    | Done a
-                    deriving Show
-
--- TODO: Distinguish between failing to parse and failing to convert.
--- The latter is recoverable.
-decode :: FromRecord a => B.ByteString -> Parser [Either String a]
-decode = decodeWith defaultDecodeOptions
-
-decodeWith :: FromRecord a => DecodeOptions -> B.ByteString
-           -> Parser [Either String a]
-decodeWith opts = go . parser
-  where
-   parser = A.parse (header $ decDelimiter opts)
-
-   go (A.Fail rest _ msg) = FailH rest err
-     where err = "parse error (" ++ msg ++ ")"
-   -- TODO: Check empty and give attoparsec one last chance to return
-   -- something:
-   go (A.Partial k)       = PartialH $ \ s -> go (k s)
-   go (A.Done rest r)     = DoneH r (decodeRecordsWith opts rest)
+------------------------------------------------------------------------
 
 -- | Have we read all available input?
 data More = Incomplete | Complete
           deriving (Eq, Show)
 
--- TODO: Do we want to expose this?
+decode :: FromRecord a => B.ByteString -> Parser a
+decode = decodeWith defaultDecodeOptions
+
 -- TODO: Call endOfLine parser once more when receiving empty input?
-decodeRecordsWith :: FromRecord a => DecodeOptions -> B.ByteString
-                  -> RecordParser [Either String a]
-decodeRecordsWith opts = go Incomplete [] . initialParser
+decodeWith :: FromRecord a => DecodeOptions -> B.ByteString -> Parser a
+decodeWith !opts = go Incomplete [] . initialParser
   where
-    go !_ acc (A.Fail rest _ msg)
+    go !_ !acc (A.Fail rest _ msg)
         | null acc  = Fail rest err
         | otherwise = Some (reverse acc) (\ s -> Fail (rest `B.append` s) err)
       where err = "parse error (" ++ msg ++ ")"
@@ -79,7 +77,7 @@ decodeRecordsWith opts = go Incomplete [] . initialParser
       where cont s = go m [] (k s)
               where m | B.null s  = Complete
                       | otherwise = Incomplete
-    go Complete _ (A.Partial _) = moduleError "decodeRecordsWith" msg
+    go Complete _ (A.Partial _) = moduleError "decodeWith" msg
         where msg = "attoparsec should never return Partial in this case"
     go m acc (A.Done rest r) 
         | B.null rest = case m of
@@ -102,36 +100,3 @@ blankLine v = V.length v == 1 && (B.null (V.head v))
 moduleError :: String -> String -> a
 moduleError func msg = error $ "Data.Csv.Streaming." ++ func ++ ": " ++ msg
 {-# NOINLINE moduleError #-}
-
-------------------------------------------------------------------------
--- * Lazy parsing
-
--- TODO: Check what lazyDecode ... :: Result String means. Overlapping
--- instance?
-
-data RecordStream a = Nil BL.ByteString
-                    | FailS BL.ByteString String
-                    | Cons a (RecordStream a)
-                    deriving (Eq, Functor, Show)
-
-data Result a = FailR BL.ByteString String
-              | DoneR Header (RecordStream (Either String a))
-              deriving (Eq, Functor, Show)
-
-lazyDecode :: FromRecord a => BL.ByteString -> Result a
-lazyDecode = lazyDecodeWith defaultDecodeOptions
-
-lazyDecodeWith :: FromRecord a => DecodeOptions -> BL.ByteString -> Result a
-lazyDecodeWith opts s0 = go (BL.toChunks s0) (decodeWith opts B.empty)
-  where
-    go ss (FailH rest err) = FailR (BL.fromChunks (rest:ss)) err
-    go [] (PartialH k)     = go [] (k B.empty)
-    go (s:ss) (PartialH k) = go ss (k s)
-    go ss (DoneH hdr p)    = DoneR hdr (go2 ss p)
-
-    go2 ss (Done xs)       = foldr Cons (Nil (BL.fromChunks ss)) xs  -- Can there be a non-empty ss here?
-    go2 ss (Fail rest err) = FailS (BL.fromChunks (rest:ss)) err
-    go2 [] (Partial k)     = go2 [] (k B.empty)
-    go2 (s:ss) (Partial k) = go2 ss (k s)
-    go2 [] (Some xs k)     = foldr Cons (go2 [] (k B.empty)) xs
-    go2 (s:ss) (Some xs k) = foldr Cons (go2 ss (k s)) xs
