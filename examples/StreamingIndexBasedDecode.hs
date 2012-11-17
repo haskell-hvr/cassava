7e13f3777a20aa9513c205182236e5342419ce60
Johan Tibell <johan.tibell@gmail.com>
2012-11-17 11:26:04 -0800

    Add example for streaming decoding to examples/

---
+++ examples/StreamingIndexBasedDecode.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import qualified Data.ByteString.Lazy as BL
+import Data.Csv.Streaming
+import Data.Foldable (for_)
+
+main :: IO ()
+main = do
+    csvData <- BL.readFile "salaries.csv"
+    -- N.B. The Foldable instance skips records that failed to
+    -- convert.
+    for_ (decode False csvData) $ \ (name, salary :: Int) ->
+        putStrLn $ name ++ " earns " ++ show salary ++ " dollars"
